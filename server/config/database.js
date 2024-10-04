4c9ae01da7ae65e58af448e601aeadb1365bfaae
matthewmillerh <matthewmillerh@outlook.com>
2024-10-04 15:49:42 +0200

    sync commit

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,18 @@
 import mysql from "mysql2";
 
 //create connectiont to database
-const db = mysql.createConnection({
-    host:"localhost",
-    user: "root",
-    password: "",
-    database: "pentiquedb"
-});
+//  const db = mysql.createConnection({
+//      host:"pentique.lovestoblog.com",
+//      user: "if0_37444679",
+//      password: "OoyV2ATMYHH2nP",
+//      database: "fif0_37444679_pentique"
+// });
+
+ const db = mysql.createConnection({
+     host:"localhost",
+     user: "root",
+     password: "",
+     database: "pentiquedb"
+ });
 
 export default db;
